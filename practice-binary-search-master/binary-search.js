eb14dec314aeee8e4bae29b33e59e01abb2fecc1
Zach Milo <zachmilo@gmail.com>
2023-05-23 19:17:42 -0400

    solved

--- practice-binary-search-master/binary-search.js
+++ practice-binary-search-master/binary-search.js
@@ -1,43 +1,39 @@
 function linearSearch (arr, target) {
 
   // Can you solve this in one line?
-  if (arr.includes(target)) {
-    return target
-  } else {
-    return -1;
-  }
-
+  return arr.includes(target) ? target : -1;
 };
 
 function binarySearch(arr, target) {
 
   // Set integers pointing to the high and low range of possible indices
   let low = 0;
-  let high = arr[arr.length - 1];
+  let high = arr.length - 1;
 
   // While high and low indices do not overlap...
   while (low <= high) {
+
     // Find the midpoint between high and low indices
-    let mid = (low + high) / 2;
+    let mid = Math.floor((low + high) / 2);
 
-
+    // If the target equals the midpoint...
+    // Return the midpoint index
     if (target === arr[mid] ) {
       return arr[mid];
     }
-    if (target > mid) {
+    // If the target is higher than the midpoint...
+      // Move the low pointer to midpoint + 1
+    if (target > arr[mid]) {
       low = mid + 1;
-    }
-    if (target < mid) {
+    } else {
       high = mid - 1;
     }
   }
   return -1
-    // Compare the target value to the midpoint value
 
-    // If the target equals the midpoint...
-      // Return the midpoint index
-    // If the target is higher than the midpoint...
-      // Move the low pointer to midpoint + 1
+
+
+
     // If the target is less than the midpoint...
       // Move the high pointer to midpoint - 1
 
