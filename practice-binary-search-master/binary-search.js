144b43b4c5f041463f3568606590fcbbc792e0c5
Zach Milo <zachmilo@gmail.com>
2023-05-23 19:10:02 -0400

    solve but failing 1 timing

--- practice-binary-search-master/binary-search.js
+++ practice-binary-search-master/binary-search.js
@@ -1,17 +1,37 @@
 function linearSearch (arr, target) {
 
   // Can you solve this in one line?
+  if (arr.includes(target)) {
+    return target
+  } else {
+    return -1;
+  }
 
 };
 
 function binarySearch(arr, target) {
 
   // Set integers pointing to the high and low range of possible indices
+  let low = 0;
+  let high = arr[arr.length - 1];
 
   // While high and low indices do not overlap...
-
+  while (low <= high) {
     // Find the midpoint between high and low indices
+    let mid = (low + high) / 2;
 
+
+    if (target === arr[mid] ) {
+      return arr[mid];
+    }
+    if (target > mid) {
+      low = mid + 1;
+    }
+    if (target < mid) {
+      high = mid - 1;
+    }
+  }
+  return -1
     // Compare the target value to the midpoint value
 
     // If the target equals the midpoint...
